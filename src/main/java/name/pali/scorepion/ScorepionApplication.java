e3ea75bd7384dd007aa39b329749ef020e04df81
Pál Földesi <pal-foldesi@users.noreply.github.com>
2021-03-28 13:40:36 +0200

    initial commit

---
+++ src/main/java/name/pali/scorepion/ScorepionApplication.java
@@ -0,0 +1,13 @@
+package name.pali.scorepion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorepionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScorepionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/name/pali/scorepion/ScorepionApplicationTests.java
@@ -0,0 +1,13 @@
+package name.pali.scorepion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorepionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
